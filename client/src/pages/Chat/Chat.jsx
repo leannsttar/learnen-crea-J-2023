9b62a560b88f1df828ad21cac1ca434f681c922c
lpzxm <88464727+lpzxm@users.noreply.github.com>
2023-07-07 13:56:39 -0600

    intento de chat responsive

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -67,9 +67,7 @@ export function Chat() {
             time: "10:30 am"
         }
     ]);
-
     const [isOnline, setIsOnline] = useState(true);
-
     const [messages, setMessages] = useState([
         {
             id: 1,
@@ -82,8 +80,8 @@ export function Chat() {
             content: 'Yo estoy bien, y tú?'
         }
     ]);
-
     const [inputValue, setInputValue] = useState('');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleChange = event => {
         setInputValue(event.target.value);
@@ -101,133 +99,172 @@ export function Chat() {
     };
 
     return (
-        <div className="container mx-auto w-">
-            <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2 ">
-                <div className="font-semibold text-black text-4xl">Messages <span className='text-[#6F84CD] '>(2)</span></div>
+        <div className="container mx-auto">
+            <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
+                <div className="font-semibold text-black text-3xl">
+                    Messages <span className="text-[#6F84CD]">(2)</span>
+                </div>
                 <div className="w-1/2 flex items-center">
-                    <img src="/assets/leandro.png" className='object-cover h-16 w-16 rounded-full' alt="" srcset="" />
+                    <img
+                        src="/assets/leandro.png"
+                        className="object-cover h-12 w-12 rounded-full"
+                        alt=""
+                    />
                     <div className="w-1/2 flex items-center">
                         <div className="p-3">
-                            <span className='text-black font-bold text-xl'>Lean Staring</span>
+                            <span className="text-black font-bold text-lg">Lean Staring</span>
                             <div className="flex items-center">
                                 <div
-                                    className="w-3 h-3 rounded-full mr-2"
-                                    style={{ backgroundColor: isOnline ? 'green' : 'red' }}
+                                    className={`w-3 h-3 rounded-full mr-2 ${isOnline ? 'bg-green-500' : 'bg-red-500'
+                                        }`}
                                 ></div>
                                 <span>{isOnline ? 'En línea' : 'Desconectado'}</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button className="h-12 w-12 text-lg p-2 rounded-full border-2 border-black text-black font-semibold flex items-center justify-center">
-                    i
+                <button
+                    className="h-10 w-10 text-lg p-2 rounded-full border-2 border-black text-black font-semibold flex items-center justify-center sm:hidden"
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                    <svg
+                        className={`h-6 w-6 ${isMenuOpen ? 'hidden' : 'block'}`}
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
+                    <svg
+                        className={`h-6 w-6 ${isMenuOpen ? 'block' : 'hidden'}`}
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M6 18L18 6M6 6l12 12"
+                        />
+                    </svg>
                 </button>
             </div>
 
-            <div className="flex flex-row justify-between bg-white">
-                <div className="flex flex-col w-2/5 border-r-2 overflow-y-auto">
-                    <div className="border-b-2 py-4 px-2">
-                        <input
-                            type="text"
-                            placeholder="Buscar chat"
-                            className="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
-                        />
-                    </div>
-
-                    <div className="flex flex-col overflow-y-auto">
-                        {users.map((user) => (
-                            <div
-                                key={user.id}
-                                className="flex flex-row py-4 px-2 items-center hover:bg-gray-100"
-                            >
-                                <div className="w-1/4">
-                                    <img
-                                        src={user.avatar}
-                                        className="object-cover h-16 w-16 rounded-full"
-                                        alt=""
-                                    />
-                                    {user.online ? (
-                                        <div className="bg-green-500 h-3 w-3 rounded-full absolute bottom-0 right-0 border-2 border-white"></div>
-                                    ) : (
-                                        <div className="bg-red-500 h-3 w-3 rounded-full absolute bottom-0 right-0 border-2 border-white"></div>
-                                    )}
-                                </div>
-                                <div className="w-full ml-4">
-                                    <div className="flex justify-between items-center">
-                                        <div className="text-lg font-semibold">{user.name}</div>
-                                        <div className="text-gray-600 text-sm">{user.time}</div>
-                                    </div>
-                                    <span className="text-gray-500">{user.message}</span>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-
+            <div className="flex flex-col bg-white">
+                <div
+                    className={`${isMenuOpen ? 'block' : 'hidden'
+                        } sm:hidden border-b-2 px-2 py-4`}
+                >
+                    <input
+                        type="text"
+                        placeholder="Buscar chat"
+                        className="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
+                    />
                 </div>
 
-                <div className="w-full px-5 flex flex-col justify-between bg-[url(/assets/chatbg.jpg)]">
-                    <div className="flex flex-col mt-5">
-                        {messages.map(message => (
-                            <div
-                                key={message.id}
-                                className={`flex justify-${message.userAvatar ===
-                                    '/assets/leandro.png'
-                                    ? 'end'
-                                    : 'start'
-                                    } mb-3`}
-                            >
+                <div className="flex flex-col sm:flex-row">
+                    <div
+                        className={`${isMenuOpen ? 'block' : 'hidden'
+                            } sm:block sm:w-2/5 border-r-2 overflow-y-auto`}
+                    >
+                        <div className="flex flex-col overflow-y-auto">
+                            {users.map(user => (
                                 <div
-                                    className={`flex ${message.userAvatar ===
-                                        '/assets/NacePhoto.jpg'
-                                        ? 'flex-row-reverse'
-                                        : 'flex-row'
-                                        } items-center`}
+                                    key={user.id}
+                                    className="flex flex-row py-4 px-2 items-center hover:bg-gray-100"
                                 >
-                                    <img
-                                        src={message.userAvatar}
-                                        className={`object-cover h-8 w-8 rounded-full ${message.userAvatar ===
-                                            '/assets/leandro.png'
-                                            ? 'ml-2'
-                                            : 'mr-2'
-                                            }`}
-                                        alt=""
-                                    />
-                                    <div
-                                        className={`bg-gray-100 py-2 px-4 rounded-full ${message.userAvatar ===
-                                            '/assets/NacePhoto.jpg'
-                                            ? 'text-right bg-[#FF8399]'
-                                            : 'text-left text-gray-800'
-                                            }`}
-                                    >
-                                        {message.content}
+                                    <div className="w-1/4">
+                                        <img
+                                            src={user.avatar}
+                                            className="object-cover h-12 w-12 rounded-full"
+                                            alt=""
+                                        />
+                                        <div
+                                            className={`bg-${user.online ? 'green' : 'red'}-500 h-3 w-3 rounded-full absolute bottom-0 right-0 border-2 border-white`}
+                                        ></div>
+                                    </div>
+                                    <div className="w-3/4 ml-4">
+                                        <div className="flex justify-between items-center">
+                                            <div className="text-lg font-semibold">{user.name}</div>
+                                            <div className="text-gray-600 text-sm">{user.time}</div>
+                                        </div>
+                                        <span className="text-gray-500">{user.message}</span>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
 
-                    <div className="flex justify-center items-center mt-5 mb-7">
-                        <form onSubmit={handleSubmit} className="w-full flex">
-                            <button
-                                type="button"
-                                className="text-white py-3 px-6 rounded-full mr-2"
-                            >
-                                <img src="/assets/imgbtn.png" className='w-11 invert' alt="" />
-                            </button>
-                            <input
-                                type="text"
-                                placeholder="Escribe tu mensaje"
-                                className="rounded-full border-2 border-[#A7A7A7] hover:border-[#FF8399] py-3 px-5 flex-1"
-                                value={inputValue}
-                                onChange={handleChange}
-                            />
-                            <button
-                                type="submit"
-                                className="text-white py-3 px-6 rounded-full mr-2"
-                            >
-                                <img src="/assets/enviarbtn.png" className='w-11 invert' alt="" />
-                            </button>
-                        </form>
+                    <div className="w-full px-5 flex flex-col bg-[url(/assets/chatbg.jpg)]">
+                        <div className="flex flex-col mt-5 sm:mt-0 sm:justify-between">
+                            {messages.map(message => (
+                                <div
+                                    key={message.id}
+                                    className={`flex justify-${message.userAvatar === '/assets/leandro.png' ? 'end' : 'start'
+                                        } mb-3`}
+                                >
+                                    <div
+                                        className={`flex ${message.userAvatar === '/assets/NacePhoto.jpg'
+                                                ? 'flex-row-reverse'
+                                                : 'flex-row'
+                                            } items-center`}
+                                    >
+                                        <img
+                                            src={message.userAvatar}
+                                            className={`object-cover h-8 w-8 rounded-full ${message.userAvatar === '/assets/leandro.png' ? 'ml-2' : 'mr-2'
+                                                }`}
+                                            alt=""
+                                        />
+                                        <div
+                                            className={`bg-gray-100 py-2 px-4 rounded-full ${message.userAvatar === '/assets/NacePhoto.jpg'
+                                                    ? 'text-right bg-[#FF8399]'
+                                                    : 'text-left text-gray-800'
+                                                }`}
+                                        >
+                                            {message.content}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-center items-center mt-5 mb-7">
+                            <form onSubmit={handleSubmit} className="w-full flex">
+                                <button
+                                    type="button"
+                                    className="text-white py-2 px-4 rounded-full mr-2"
+                                >
+                                    <img
+                                        src="/assets/imgbtn.png"
+                                        className="w-8 h-8 invert"
+                                        alt=""
+                                    />
+                                </button>
+                                <input
+                                    type="text"
+                                    placeholder="Escribe tu mensaje"
+                                    className="rounded-full border-2border-[#A7A7A7] hover:border-[#FF8399] py-2 px-3 flex-1"
+                                    value={inputValue}
+                                    onChange={handleChange}
+                                />
+                                <button
+                                    type="submit"
+                                    className="text-white py-2 px-4 rounded-full mr-2"
+                                >
+                                    <img
+                                        src="/assets/enviarbtn.png"
+                                        className="w-8 h-8 invert"
+                                        alt=""
+                                    />
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
