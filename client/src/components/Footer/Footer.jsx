c09d3c4bc47ed20d6b632db8af33e09b2c1b0028
Rodrogas666 <105886719+Rodrogas666@users.noreply.github.com>
2023-07-09 19:38:10 -0600

    footer y notificaciones, puta no avancé xd
    mañana se empiezan los forms, sex

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-export function Footer(){
-    return(
-
-        <>
-        
-        <div className='bg-[#1E2833]'>
-            <div className='flex flex-col items-center pt-16 pb-20 bottom-0 w-full'>
-                <img className='w-[104px] h-[104px]' src="assets/learnen.png" alt="" />
-                <p className='text-white text-center pt-8 pb-8 text-lg italic'>La red social donde todo el mundo <br />puede aprender y practicar.</p>
-                <div>
-                    <ul className='text-white flex flex-row pt-10 gap-32'>
-                        <li className='text-base'>Home</li>
-                        <li className='text-base'>Posts</li>
-                        <li className='text-base'>Comunidad</li>
-                        <li className='text-base'>Chat</li>
-                        <li className='text-base'>Blog</li>
-                        <li className='text-base'>Notificaciones</li>
-                    </ul>
-                </div>
-                <div className='flex flex-row pt-14 gap-28'>
-                    <img className='w-full h-[22px]' src="/assets/facebook.png" alt="" />
-                    <img className='w-full h-[22px]' src="/assets/instagram.png" alt="" />
-                    <img className='w-full h-[22px]' src="/assets/twitter.png" alt="" />
-                </div>
-            </div>
+export function Footer() {
+  return (
+    <>
+      <div className="bg-[#1E2833]">
+        <div className="flex flex-col items-center pt-16 pb-20 bottom-0 w-full">
+          <img className="w-[104px] h-[104px]" src="assets/learnen.png" alt="" />
+          <p className="text-white text-center pt-8 pb-8 text-lg italic">
+            La red social donde todo el mundo <br />puede aprender y practicar.
+          </p>
+          <div className="lg:flex">
+            <ul className="text-white flex flex-col lg:flex-row lg:gap-12 pt-10">
+              <li className="text-base">Home</li>
+              <li className="text-base">Posts</li>
+              <li className="text-base">Comunidad</li>
+              <li className="text-base">Chat</li>
+              <li className="text-base">Blog</li>
+              <li className="text-base">Notificaciones</li>
+            </ul>
+          </div>
+          <div className="flex flex-row pt-14 gap-8">
+            <img className="w-[19px] h-[19px]" src="/assets/facebook.png" alt="" />
+            <img className="w-[19px] h-[19px]" src="/assets/instagram.png" alt="" />
+            <img className="w-[19px] h-[19px]" src="/assets/twitter.png" alt="" />
+          </div>
         </div>
-        
-        </>
-
-    )
+      </div>
+    </>
+  );
 }
-  
-  
-  
