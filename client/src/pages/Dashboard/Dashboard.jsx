695cc0fd66172bc1f7e898422d4fef091d0397d4
vlado <87096620+Pirulindekanto@users.noreply.github.com>
2023-07-17 16:01:37 -0600

    nadita importante xd

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 export function Dashboard() { 
     return(
-        <h1>Dashboard Admin</h1>
+        <>
+        
+        <div className='bg-green-200 grid grid-cols-[1fr_300px]'>
+            <div className='bg-red-200'>
+                <h1>Hola</h1>
+            </div>
+            <div className='bg-yellow-200'>
+            <h1>Hola</h1>
+
+            </div>
+        </div>
+        
+        </>
     )
 }
