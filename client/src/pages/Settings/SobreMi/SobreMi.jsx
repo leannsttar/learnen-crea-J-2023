c494fcdee99c250df5813379befc8fbec7451976
Ferchofloo <105887401+leannsttar@users.noreply.github.com>
2023-07-20 16:07:12 -0600

    clase

--- client/src/pages/Settings/SobreMi/SobreMi.jsx
+++ client/src/pages/Settings/SobreMi/SobreMi.jsx
@@ -1,6 +1,7 @@
 import { React, useState, useRef, useEffect } from "react";
 import { SobreMiData } from "./SobreMiData";
 import { EditButton } from "./EditButton";
+import editProfile from "../../../assets/editProfile.svg";
 
 export function SobreMi() {
   const sobreMiUserData = {
@@ -10,38 +11,64 @@ export function SobreMi() {
   };
 
   let [onEdit, setOnEdit] = useState(false);
+  let [notEdit, setNotEdit] = useState(true);
 
   function yesOnEdit() {
     setOnEdit(true);
+    setNotEdit(false)
     // inputRef.current.focus();
   }
 
   function notOnEdit() {
     setOnEdit(false);
+    setNotEdit(true)
+
   }
 
   return (
     <>
-        <EditButton
+      {/* <EditButton
           onEdit={onEdit ? true : false}
           setEdit={() => setOnEdit(true)}
           setNotEdit={() => setOnEdit(false)}
           size="w-7"
           fontSize={"text-[25px]"}
-        />
+        /> */}
+      {onEdit ? (
+        <div className="flex gap-5 text-[19px]">
+          <button onClick={() => notOnEdit()} className="font-bold  ">
+            Cancelar
+          </button>
+          <button className="bg-[#FF8399] rounded-3xl text-white py-1 px-3 ">
+            Guardar
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={() => yesOnEdit()}
+          className="flex gap-3 items-center font-[600] text-[16px]"
+        >
+          <img src={editProfile} alt="" className="w-7" />
+          <div className="text-[25px]">
+            <p className="text-[#FF8399]">Editar</p>
+          </div>
+        </button>
+      )}
       <div className="flex flex-col gap-10 mt-10 text-[20px]">
         <SobreMiData
-        
+          notEdit={notEdit ? true : false}
           onEdit={onEdit ? true : false}
           label="Describe cómo eres"
           data={sobreMiUserData.comoEres}
         />
         <SobreMiData
+          notEdit={notEdit ? true : false}
           onEdit={onEdit ? true : false}
           label="Cuáles son tus metas para aprender idiomas?"
           data={sobreMiUserData.tusMetas}
         />
         <SobreMiData
+          notEdit={notEdit ? true : false}
           onEdit={onEdit ? true : false}
           label="De qué te gusta hablar?"
           data={sobreMiUserData.gustosConversacion}
--- client/src/pages/Settings/SobreMi/SobreMiData.jsx
+++ client/src/pages/Settings/SobreMi/SobreMiData.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useRef, useEffect } from "react";
 
-export function SobreMiData({ label, data, onEdit }) {
+export function SobreMiData({ label, data, onEdit, notEdit }) {
 
     const [inputValue, setInputValue] = useState(data);
 
@@ -8,8 +8,13 @@ export function SobreMiData({ label, data, onEdit }) {
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value) 
-        
     };
+
+    const resetData = () => {
+      setInputValue(data)
+    }
+
+    
 
   return (
     <div className="">
@@ -17,7 +22,7 @@ export function SobreMiData({ label, data, onEdit }) {
       {onEdit ? (
         <input
           value={inputValue}
-          onChange={handleInputChange}
+          onChange={notEdit ? resetData : handleInputChange}
           //Esto se pone para tipo referenciar el Ref XD, nose
           // onBlur={handleInputBlur}
           className="font-[300] bg-[#e2e2e2a9] rounded-md px-2 outline-none w-full"
