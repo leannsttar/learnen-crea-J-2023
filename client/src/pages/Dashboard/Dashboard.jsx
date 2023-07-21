236421fdfa135f913af23943298e6e7e5ebab05c
Rodrogas666 <rodri123pineda@gmail.com>
2023-07-21 11:42:35 -0600

    medio dashboard xd, que weba xd

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-export function Dashboard () {
+export function Dashboard() {
   return (
     <div className="flex h-screen">
       {/* Aside */}
       <aside className="w-1/5 bg-green-500 p-4">
-        
-      <div className="flex flex-row">
-      <img className="w-[90px] h-[90px]" src="/assets/learnen.png" alt="" />
-        <img className="m-3 w-[95px] h-[30px]" src="src/assets/learnen-palabra.jpg" alt="" />
-      </div>
+
+        <div className="flex flex-row">
+          <img className="w-[90px] h-[90px]" src="/assets/learnen.png" alt="" />
+          <img className="m-3 w-[95px] h-[30px]" src="src/assets/learnen-palabra.jpg" alt="" />
+        </div>
         <div className="mb-8">
           <button className="w-full py-2 px-4 bg-blue-500 text-white mb-2 rounded">
             General
@@ -21,7 +21,7 @@ export function Dashboard () {
             <button className="w-full py-2 px-4 bg-blue-500 text-white rounded">
               Opciones
             </button>
-            <div className="dropdown-content bg-black p-2">
+            <div className="dropdown-content bg-gray-200 p-2">
               <a href="#">Opción 1</a>
               <a href="#">Opción 2</a>
               <a href="#">Opción 3</a>
@@ -39,7 +39,7 @@ export function Dashboard () {
         {/* Navbar */}
         <nav className="flex justify-between mb-4 bg-yellow-500">
           <div className="text-xl font-bold">Hola Rodri</div>
-          <div className="flex items-center">
+          <div className="flex items-center justify-center">
             <img
               className="w-8 h-8 rounded-full mr-2"
               src="url_de_la_imagen_de_perfil.jpg"
@@ -56,37 +56,81 @@ export function Dashboard () {
         <div className="flex">
           {/* Columna izquierda */}
           <div className="w-1/2 p-4">
-            <div className="mb-4">
-              <h1 className="text-xl font-bold mb-2">Usuarios</h1>
-              <h1 className="text-2xl">100</h1>
+
+            <div className="bg-yellow-400 flex p-2">
+              <div className="w-1/2 mb-4 bg-slate-300 m-2">
+                <div className="flex items-center justify-center">
+                  <img src="src/assets/users-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                  <div>
+                    <h1 className="text-xl font-bold mb-2">Usuarios</h1>
+                    <h1 className="text-2xl">100</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/2 mb-4 bg-slate-300 m-2">
+                <div className="flex items-center justify-center">
+                  <img src="src/assets/file-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                  <div>
+                    <h1 className="text-xl font-bold mb-2">Publicaciones</h1>
+                    <h1 className="text-2xl">678</h1>
+                  </div>
+                </div>
+              </div>
             </div>
+            <div className="bg-yellow-400 flex">
+              <div className="w-1/2 mb-4 bg-slate-300 m-2">
+                <div className="flex items-center justify-center">
+                  <img src="src/assets/reports-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                  <div>
+                    <h1 className="text-xl font-bold mb-2">Reportes</h1>
+                    <h1 className="text-2xl">45</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/2 mb-4 bg-slate-300 m-2">
+                <div className="flex items-center justify-center">
+                  <img src="src/assets/articles-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                  <div>
+                    <h1 className="text-xl font-bold mb-2">Artículos</h1>
+                    <h1 className="text-2xl">345</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
             <div>
               <div className="text-xl font-bold mb-2">Administradores</div>
-              <div className="p-4 bg-white rounded">
+              <div className="p-4 rounded bg-pink-300 flex flex-col">
+                <button className="bg-red-500 text-white rounded p-1">
+                  X
+                </button>
                 <img
                   className="w-16 h-16 rounded-full mb-2"
                   src="url_de_la_imagen_admin.jpg"
                   alt="Foto de administrador"
                 />
                 <div className="text-xl mb-2">Nombre Admin</div>
-                <button className="bg-red-500 text-white rounded p-1">
-                  X
-                </button>
+                <div className="text-base mb-2">Diseñador</div>
               </div>
             </div>
           </div>
 
           {/* Columna derecha */}
           <aside className="w-1/2 p-4">
-            <div className="mb-4">
-              <div className="text-xl font-bold mb-2 bg-red-500">Last Users
-              <img
-                className="w-12 h-12 rounded-full mb-2"
-                src="url_de_la_imagen_usuario.jpg"
-                alt="Foto de último usuario"
-              />
-              <div className="text-lg font-medium mb-2">Nombre Usuario</div>
-              <div className="text-sm text-gray-500">correo@example.com</div>
+            <div className="mb-4 bg-red-500">
+              <h1 className="text-2xl">Últimos usuarios</h1>
+              <div className="text-xl font-bold">
+                <img
+                  className="w-12 h-12 rounded-full mb-2"
+                  src="url_de_la_imagen_usuario.jpg"
+                  alt="Foto de último usuario"
+                />
+                <div className="text-lg font-medium mb-2">Nombre Usuario</div>
+                <div className="text-sm text-gray-500">correo@example.com</div>
               </div>
             </div>
           </aside>
