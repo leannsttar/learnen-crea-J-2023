b1ec0a86d5bfbbd1ddb26b61ace066a8a4525c27
Rodrogas666 <>
2023-07-24 16:06:21 -0600

    medio dashboard y mejoras, help god

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export function Dashboard() {
+
   return (
     <div className="flex h-screen">
       {/* Aside */}
-      <aside className="w-1/5 bg-green-500 p-4">
+      <aside className="w-1/5 bg-green-500 pl-4 pr-8 mr-4">
 
         <div className="flex flex-row">
           <img className="w-[90px] h-[90px]" src="/assets/learnen.png" alt="" />
@@ -55,85 +56,83 @@ export function Dashboard() {
         {/* Content */}
         <div className="flex">
           {/* Columna izquierda */}
-          <div className="w-1/2 p-4">
+          <div className="w-1/2 pl-8">
 
             <div className="bg-yellow-400 flex p-2">
-              <div className="w-1/2 mb-4 bg-slate-300 m-2">
-                <div className="flex items-center justify-center">
-                  <img src="src/assets/users-div.png" alt="Ícono" className="w-8 h-8 m-2" />
-                  <div>
-                    <h1 className="text-xl font-bold mb-2">Usuarios</h1>
-                    <h1 className="text-2xl">100</h1>
-                  </div>
+              <div className="flex-1 mb-4 bg-slate-300 m-2 flex items-center p-4 gap-2">
+                <img src="src/assets/users-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                <div className="flex flex-row gap-2 items-center">
+                  <h1 className="text-lg mb-0">Usuarios</h1>
+                  <h1 className="text-base font-bold mb-0">100</h1>
                 </div>
               </div>
-              <div className="w-1/2 mb-4 bg-slate-300 m-2">
-                <div className="flex items-center justify-center">
-                  <img src="src/assets/file-div.png" alt="Ícono" className="w-8 h-8 m-2" />
-                  <div>
-                    <h1 className="text-xl font-bold mb-2">Publicaciones</h1>
-                    <h1 className="text-2xl">678</h1>
-                  </div>
+              <div className="flex-1 mb-4 bg-slate-300 m-2 flex items-center p-4 gap-2">
+                <img src="src/assets/file-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                <div className="flex flex-row gap-2 items-center">
+                  <h1 className="text-lg mb-0">Publicaciones</h1>
+                  <h1 className="text-base font-bold mb-0">678</h1>
                 </div>
               </div>
             </div>
             <div className="bg-yellow-400 flex">
-              <div className="w-1/2 mb-4 bg-slate-300 m-2">
-                <div className="flex items-center justify-center">
-                  <img src="src/assets/reports-div.png" alt="Ícono" className="w-8 h-8 m-2" />
-                  <div>
-                    <h1 className="text-xl font-bold mb-2">Reportes</h1>
-                    <h1 className="text-2xl">45</h1>
-                  </div>
+              <div className="flex-1 mb-4 bg-slate-300 m-2 flex items-center p-4 gap-2">
+                <img src="src/assets/reports-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                <div className="flex flex-row gap-2 items-center">
+                  <h1 className="text-lg mb-0">Reportes</h1>
+                  <h1 className="text-base font-bold mb-0">45</h1>
                 </div>
               </div>
-              <div className="w-1/2 mb-4 bg-slate-300 m-2">
-                <div className="flex items-center justify-center">
-                  <img src="src/assets/articles-div.png" alt="Ícono" className="w-8 h-8 m-2" />
-                  <div>
-                    <h1 className="text-xl font-bold mb-2">Artículos</h1>
-                    <h1 className="text-2xl">345</h1>
-                  </div>
+              <div className="flex-1 mb-4 bg-slate-300 m-2 flex items-center p-4 gap-2">
+                <img src="src/assets/articles-div.png" alt="Ícono" className="w-8 h-8 m-2" />
+                <div className="flex flex-row gap-2 items-center">
+                  <h1 className="text-lg mb-0">Artículos</h1>
+                  <h1 className="text-base font-bold mb-0">345</h1>
                 </div>
               </div>
             </div>
 
 
 
-
-
-            <div>
-              <div className="text-xl font-bold mb-2">Administradores</div>
-              <div className="p-4 rounded bg-pink-300 flex flex-col">
-                <button className="bg-red-500 text-white rounded p-1">
-                  X
-                </button>
-                <img
-                  className="w-16 h-16 rounded-full mb-2"
-                  src="url_de_la_imagen_admin.jpg"
-                  alt="Foto de administrador"
-                />
-                <div className="text-xl mb-2">Nombre Admin</div>
-                <div className="text-base mb-2">Diseñador</div>
+            <div className="mt-16">
+              <div>
+                <div className="text-xl font-bold mb-2">Administradores</div>
+                <div className="relative p-4 rounded bg-pink-300 flex flex-col items-center w-52 gap-2">
+                  <button className="absolute top-0 right-0 text-gray rounded-full p-2">
+                    X
+                  </button>
+                  <img
+                    className="w-16 h-16 rounded-full mb-2"
+                    src="url_de_la_imagen_admin.jpg"
+                    alt="Foto de administrador"
+                  />
+                  <div className="text-base mb-2 text-center">Rodrigo <br /> Pineda</div>
+                  <div className="text-sm mb-2">Diseñador</div>
+                </div>
               </div>
             </div>
+
+
           </div>
 
           {/* Columna derecha */}
-          <aside className="w-1/2 p-4">
-            <div className="mb-4 bg-red-500">
+          <aside className="w-1/2 pr-16 pl-16">
+
+            <div className="mb-4 bg-red-500 pl-4 pr-4 pt-2 pb-4 rounded">
               <h1 className="text-2xl">Últimos usuarios</h1>
-              <div className="text-xl font-bold">
+              <div className="flex items-center mt-4">
                 <img
-                  className="w-12 h-12 rounded-full mb-2"
+                  className="w-12 h-12 rounded-full mr-4"
                   src="url_de_la_imagen_usuario.jpg"
                   alt="Foto de último usuario"
                 />
-                <div className="text-lg font-medium mb-2">Nombre Usuario</div>
-                <div className="text-sm text-gray-500">correo@example.com</div>
+                <div className="">
+                  <div className="text-sm font-medium mb-2">Nombre Usuario</div>
+                  <div className="text-sm text-gray-500">correo@example.com</div>
+                </div>
               </div>
             </div>
           </aside>
+
         </div>
       </div>
     </div>
--- client/src/pages/Notificaciones/Notificaciones.jsx
+++ client/src/pages/Notificaciones/Notificaciones.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { LiaCommentSolid } from 'react-icons/lia';
 
 
 export function Notificaciones()  {
   return (
     <>
-    <div className="flex items-center m-8 bg-gray-100 border-b border-black p-2">
+       <h1 className="text-3xl font-bold text-pink-400 ml-16 mt-24">
+            Notificaciones
+          </h1>
+    <div className="flex items-center ml-10 mr-10 mt-14 mb-8 bg-gray-100 border-b border-zinc-400 p-2 rounded-xl">
       <div className="flex-shrink-0">
-        <LiaCommentSolid className='w-8 h-8'/>
+        <img src="src/assets/message-notif.png" alt="" className='w-6 h-6 mr-2 ml-2' />
       </div>
       <div className="ml-2">
         <img className="w-8 h-8 rounded-full" src="/assets/person-post.png" alt="Avatar" />
@@ -17,7 +19,7 @@ export function Notificaciones()  {
         <p className="text-xs text-gray-500">4 minutos</p>
       </div>
       <div className="flex-grow"></div>
-      <div className="w-3 h-3 bg-pink-500 rounded-full mr-4"></div>
+      <div className="w-3 h-3 bg-pink-300 rounded-full mr-4"></div>
     </div>
     </>
   );
