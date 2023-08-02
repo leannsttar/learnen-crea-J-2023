0440c3b2e1214b5a3258455900da3cd327eb9399
Rodrogas666 <105886719+Rodrogas666@users.noreply.github.com>
2023-08-01 23:01:47 -0600

    puta sí jodí todo, mañana se termina

--- client/src/pages/Notificaciones/Notificaciones.jsx
+++ client/src/pages/Notificaciones/Notificaciones.jsx
@@ -1,35 +1,62 @@
 import React from "react";
 
 export function Notificaciones() {
+  const dataNotificaciones = [
+    {
+      id: 1,
+      persona: "Sergio",
+      fotoPersona: "/assets/person-post.png",
+      icono: "src/assets/message-notif.png",
+      tiempo: "4 minutos",
+    },
+    {
+      id: 1,
+      persona: "Leandro",
+      fotoPersona: "/assets/person-post.png",
+      icono: "src/assets/gallery-add.png",
+      tiempo: "4 minutos",
+    },
+    {
+      id: 1,
+      persona: "Saz",
+      fotoPersona: "/assets/person-post.png",
+      icono: "src/assets/Heart.png",
+      tiempo: "4 minutos",
+    },
+    {
+      id: 1,
+      persona: "Nacely",
+      fotoPersona: "/assets/person-post.png",
+      icono: "src/assets/profile-add.png",
+      tiempo: "4 minutos",
+    },
+  ];
   return (
     <>
       <h1 className="text-3xl font-bold text-pink-400 ml-16 mt-32">
         Notificaciones
       </h1>
-      <div className="flex items-center ml-10 mr-10 mt-14 mb-8 bg-gray-100 border-b border-zinc-400 p-2 rounded-xl">
-        <div className="flex-shrink-0">
-          <img
-            src="src/assets/message-notif.png"
-            alt=""
-            className="w-6 h-6 mr-2 ml-2"
-          />
+      {dataNotificaciones.map((data) => (
+        <div
+          className="flex items-center ml-10 mr-10 mt-14 mb-14 bg-gray-100 border-b border-zinc-400 p-4 rounded-xl"
+          key={data.id}
+        >
+          <div className="flex-shrink-0">
+            <img src={data.icono} alt="" className="w-6 h-6 mr-2 ml-2" />
+          </div>
+          <div className="ml-2">
+            <img className="w-8 h-8 rounded-full" src={data.fotoPersona} alt="Avatar" />
+          </div>
+          <div className="ml-2">
+            <h3 className="text-base font-medium text-gray-900">
+              {data.persona}
+            </h3>
+            <p className="text-xs text-gray-500">{data.tiempo}</p>
+          </div>
+          <div className="flex-grow"></div>
+          <div className="w-3 h-3 bg-pink-300 rounded-full mr-4"></div>
         </div>
-        <div className="ml-2">
-          <img
-            className="w-8 h-8 rounded-full"
-            src="/assets/person-post.png"
-            alt="Avatar"
-          />
-        </div>
-        <div className="ml-2">
-          <h3 className="text-base font-medium text-gray-900">
-            Sergio comentó
-          </h3>
-          <p className="text-xs text-gray-500">4 minutos</p>
-        </div>
-        <div className="flex-grow"></div>
-        <div className="w-3 h-3 bg-pink-300 rounded-full mr-4"></div>
-      </div>
+      ))}
     </>
   );
 }
