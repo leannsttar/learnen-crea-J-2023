451ee1862dbb1a1ce579945431ec073adff6898f
lpzxm <ja044840@gmail.com>
2023-08-03 18:22:14 -0600

    xd

--- client/src/pages/Settings/Likes/Likes.jsx
+++ client/src/pages/Settings/Likes/Likes.jsx
@@ -3,51 +3,51 @@ import React from "react";
 const userLikes = [
   {
     id: 2901,
-    picture: "src/assets/Posts/post1.jpg",
+    picture: "/src/assets/Posts/post1.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post2.jpg",
+    picture: "/src/assets/Posts/post2.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post3.jpg",
+    picture: "/src/assets/Posts/post3.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post4.jpg",
+    picture: "/src/assets/Posts/post4.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post5.jpg",
+    picture: "/src/assets/Posts/post5.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post6.jpg",
+    picture: "/src/assets/Posts/post6.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post7.jpg",
+    picture: "/src/assets/Posts/post7.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post8.jpg",
+    picture: "/src/assets/Posts/post8.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post9.jpg",
+    picture: "/src/assets/Posts/post9.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post10.jpg",
+    picture: "/src/assets/Posts/post10.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post11.jpg",
+    picture: "/src/assets/Posts/post11.jpg",
   },
   {
     id: 2901,
-    picture: "src/assets/Posts/post12.jpg",
+    picture: "/src/assets/Posts/post12.jpg",
   },
 ];
 
