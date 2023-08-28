1223ce2d321a9c892b5f680b02d20a99b7b02594
Ferchofloo <105886720+Ferchofloo@users.noreply.github.com>
2023-08-27 23:08:41 -0600

    sii

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -348,7 +348,7 @@ export function Header() {
 
             {isLoggedIn ? (
               <div className="lg:flex lg:gap-5 lg:items-center hidden ">
-                <NotificationsMenu />
+                {/* <NotificationsMenu /> */}
                 {/* <Link
                   to={"/notifications"}
                   className="rounded-full hover:bg-[#e6e6e6] p-1"
